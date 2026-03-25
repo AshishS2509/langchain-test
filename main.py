bc4d9e404afab0c1e4eb044d96bdda32b105c07f
AshishS2509 <ashish.solanki.2509@gmail.com>
2026-03-25 23:53:52 +0530

    use lamma3 locally

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from os import environ
-
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM
+
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain-course!")
-    print(environ.get("OPENAI_API_KEY"))
+    information = """
+    Chanakya (flourished 300 bce) was a Hindu statesman and philosopher who wrote a classic treatise on polity, Artha-shastra (“The Science of Material Gain”), a compilation of almost everything that had been written in India up to his time regarding artha (property, economics, or material success).
 
+    He was born into a Brahman family and received his education at Taxila (now in Pakistan). He is known to have had a knowledge of medicine and astrology, and it is believed he was familiar with elements of Greek and Persian learning introduced into India by Zoroastrians. Some authorities believe he was a Zoroastrian or at least was strongly influenced by that religion.
+
+    Chanakya became a counselor and adviser to Chandragupta (reigned c. 321–c. 297), founder of the Mauryan empire of northern India, but lived by himself. He was instrumental in helping Chandragupta overthrow the powerful Nanda dynasty at Pataliputra, in the Magadha region.
+    """
+
+    summary_prompt = """
+    given this information: {information} about the person, I want you to create:
+    1. A short summary.
+    2. two interesting facta about the person.
+"""
+    summary_prompt_template = PromptTemplate(input_variables={"information"}, template= summary_prompt)
+    llm = OllamaLLM(model="llama3")
+    chain = summary_prompt_template | llm
+    resp = chain.invoke(input={"information": information})
+    print(resp)
 
 if __name__ == "__main__":
     main()
